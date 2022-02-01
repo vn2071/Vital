dc99ae1e55e8efddf9e03e15dc800754b658a872
vn2071 <96445892+vn2071@users.noreply.github.com>
2022-02-01 17:29:57 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -14,10 +14,10 @@ def welcome_assignment_answers(question):
                      #answer = "No"
     #elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
                     #answer = "42b76fe51778764973077a5a94056724"
-    if question == "Is MD5 a secured hashing algorithm? - Yes/No":
-                    answer = "No"
-   # elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-                   # answer = 1
+    #elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+                    #answer = "No"
+    if question ==  "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
+                    answer = 1
    # elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
               #      answer = 2
     return answer
