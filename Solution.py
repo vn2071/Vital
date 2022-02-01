601fcce5b60fc990b131b037b3a37a04c4acba89
vn2071 <96445892+vn2071@users.noreply.github.com>
2022-02-01 13:11:17 -0500

    Add files via upload

--- Solution.py
+++ Solution.py
@@ -2,7 +2,6 @@
 # Input - All eight questions given in the assignment.
 # Output - The right answer for the specific question.
 def welcome_assignment_answers(question):
-    global answer
     if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "mtls"
     elif question == "Are encoding and encryption the same? - Yes/No":
