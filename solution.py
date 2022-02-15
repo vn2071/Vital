0d03f91db777162fc9aafd28f43319b71f77018b
vn2071 <96445892+vn2071@users.noreply.github.com>
2022-02-15 13:55:59 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import sys
 def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
   #Prepare a server socket
-  serverSocket.bind(("", port))
+  serverSocket.bind(("127.0.0.1", port))
   serverSocket.listen(1)
   while True:
     #Establish the connection
