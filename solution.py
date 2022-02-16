2359efc17e1f8ecd5e5d25a120aada5eeb81b196
vn2071 <96445892+vn2071@users.noreply.github.com>
2022-02-15 21:45:52 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -16,7 +16,7 @@ def webServer(port=13331):
         message = connectionSocket.recv(1024)
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = f.read().decode()
+        outputdata = f.read()
         #Send one HTTP header line into socket.
         connectionSocket.send('\nHTTP/1.1 200 OK\n\n'.encode())
         for i in range(0, len(outputdata)):
